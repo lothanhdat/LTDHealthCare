40fe0891e4e635313bf6db5c41c2d0cab540212b
LoThanhDat <lothanhdat95@gmail.com>
2023-08-20 10:57:19 +0700

    update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// add this line for using process.env.
-// require('dotenv').config();
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
