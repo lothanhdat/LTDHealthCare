0e569dc85041d7c1cce3e48a2bf91724002f7e23
LoThanhDat <lothanhdat95@gmail.com>
2023-09-20 21:29:20 +0700

    update from home to

--- src/models/user.js
+++ src/models/user.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     phoneNumber: DataTypes.STRING,
     gender: DataTypes.BOOLEAN,
     image: DataTypes.STRING,
-    roldId: DataTypes.STRING,
+    roleId: DataTypes.STRING,
     positionId: DataTypes.STRING
 
   }, {
