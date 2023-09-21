872875513cf5d2b55ee256254d53010d2c31defa
LoThanhDat <lothanhdat95@gmail.com>
2023-09-21 21:48:50 +0700

    update create

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,7 @@
 import { json } from 'body-parser';
-import db from '../models/index'
+import db from '../models/index';
+import CRUDService from '../services/CRUDService';
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -19,8 +21,9 @@ let getAboutPage = (req, res) => {
 let getCRUD = (req, res) => {
     return res.render('test/crud.ejs');
 };
-let postCRUD = (req, res) => {
-    console.log(req.body);
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
     return res.send('post crud from server');
 }
 module.exports = {
